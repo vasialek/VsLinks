86dad660b1dbdb1620743cbab31e77fdb1cdd99e
Aleksej Vasinov <p.roglamer@gmail.com>
2019-04-12 15:52:13 +0300

    Categories repository to get active categories.

--- data/categoryRepository.go
+++ data/categoryRepository.go
@@ -1,8 +1,15 @@
 package data
 
 import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+
 	"github.com/vasialek/VsLinks/models"
 	"github.com/zabawaba99/firego"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 // CategoryRepository provides access to category table
@@ -16,15 +23,46 @@ func NewCategoryRepository() *CategoryRepository {
 }
 
 // GetAllActive returns list of categories for links
-func (cr *CategoryRepository) GetAllActive() ([]models.LinkCategory, *error) {
-	list := make([]models.LinkCategory, 1)
-	list = append(list, models.LinkCategory{
-		LinkCategoryID: "123-456-789",
-		Name:           "Fake category",
-	})
-	return list, nil
+func (cr *CategoryRepository) GetAllActive() ([]models.LinkCategory, error) {
+	fb, err := cr.getDatabaseApp()
+	if err != nil {
+		log.Printf("CategoryRepository::GetAllActive: Error connecting to Firebase. %s\n", err)
+		return nil, err
+	}
+
+	val := map[string]models.LinkCategory{}
+	if err = fb.Child("category").Value(&val); err != nil {
+		log.Printf("CategoryRepository::GetAllActive: %s\n", err)
+		return nil, errors.New("Error getting list of active categories")
+	}
+
+	log.Printf("got %d active categories\n", len(val))
+	categories := make([]models.LinkCategory, len(val))
+	pos := 0
+
+	for _, v := range val {
+		categories[pos] = v
+		pos++
+	}
+
+	return categories, nil
 }
 
-func (cr *CategoryRepository) getDatabaseApp() (*firego.Firebase, *error) {
-	return nil, nil
+func (cr *CategoryRepository) getDatabaseApp() (*firego.Firebase, error) {
+	if cr.db == nil {
+		fmt.Println("Connecting to Firebase...")
+		d, err := ioutil.ReadFile("./keys/vsm-links-db-firebase-adminsdk-gubgg-98be377e48.json")
+		if err != nil {
+			log.Printf("getDatabaseApp: can't read Firebase key: %s\n", err)
+			return nil, err
+		}
+
+		conf, err := google.JWTConfigFromJSON(d, "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/firebase.database")
+		if err != nil {
+			return nil, err
+		}
+
+		cr.db = firego.New("https://vsm-links-db.firebaseio.com/", conf.Client(oauth2.NoContext))
+	}
+	return cr.db, nil
 }
